8a526c9ba51f4667834cca26e4bd889afda17c74
Bruno Marco <brunomarcodasilva@gmail.com>
2022-03-30 22:26:33 -0300

    add programs

---
+++ aula03/saber-maior-menor-2n.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <locale.h>
+
+int main (void){
+
+    setlocale(LC_ALL,"Portuguese");
+
+    float n[2];
+    int i = 0;
+
+    for ( i = 0 ; i <= 1 ; i++) {
+
+        printf("Digite o %iº número: ", i+1);
+        printf("Digite o %iº número: ", i+1);
+        scanf("%f", &n[i]);
+
+    }
+
+    if ( n[0] == n[1] ){
+
+        printf("Os números são iguais!");
+
+    } else {
+
+        n[0]>n[1]? printf("\nO número: %.2f é a maior número dentre os números digitados!", n[0]) : printf("\nO número: %.2f é a maior número dentre os números digitados!", n[1]);
+	    n[0]<n[1]? printf("\nO número: %.2f é a menor número dentre os números digitados!", n[0]) : printf("\nO número: %.2f é a menor número dentre os números digitados!", n[1]);
+
+    }
+
+    return (0);
+}
---
+++ aula03/saber-maior-menor-3n.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <locale.h>
+
+int main (void){
+
+    setlocale(LC_ALL,"Portuguese");
+
+    float n[2];
+    int i = 0;
+
+    for ( i = 0 ; i <= 1 ; i++) {
+
+        printf("Digite o %iº número: ", i+1);
+        scanf("%f", &n[i]);
+
+    }
+
+    // printf("Digite o primeiro número: ");
+    // scanf("%f", &n[0]);
+
+    // printf("Digite o segundo número: ");
+    // scanf("%f", &n[1]);
+	
+	n[0]>n[1]? printf("\nO número: %.2f é a maior número dentre os números digitados!", n[0]) : printf("\nO número: %.2f é a maior número dentre os números digitados!", n[1]);
+	n[0]<n[1]? printf("\nO número: %.2f é a menor número dentre os números digitados!", n[0]) : printf("\nO número: %.2f é a menor número dentre os números digitados!", n[1]);
+
+    return (0);
+}
