72f6070f924c903c621b7ecc8626630bcb7d0136
Bruno Marco <brunomarcodasilva@gmail.com>
2022-05-01 23:09:13 -0300

    new projects

---
+++ Livro Algoritmos/cap3/letra3numeroC.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <conio.h>
+#include <locale.h>
+#include <math.h>
+using namespace std;
+
+//x=m*(a*h+(v^2/2))
+int main(){
+	setlocale(LC_ALL,"Portuguese");
+	
+	
+	
+	system("pause");
+	return 0;
+}
---
+++ Livro Algoritmos/cap3/letra3numeroD.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <conio.h>
+#include <locale.h>
+#include <math.h>
+using namespace std;
+
+//d=v*t+(a*t^2/2)
+int main(){
+	setlocale(LC_ALL,"Portuguese");
+	
+	
+	
+	system("pause");
+	return 0;
+}
